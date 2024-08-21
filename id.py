ee4afa0320fac393ab37cc0c5aade792d7ce4d3a
jp$ <33301009+jplamadrid@users.noreply.github.com>
2024-08-21 11:36:36 +0800

    Update id.py

--- id.py
+++ id.py
@@ -1,16 +1,25 @@
-# Write your code below
-class Employee:
+class Employee():
   new_id = 1
-
   def __init__(self):
     self.id = Employee.new_id
     Employee.new_id += 1
-  
+
   def say_id(self):
-    print("My id is", self.id)
-  
+    print("My id is {}.".format(self.id))
+
+class Admin(Employee):
+  def say_id(self):
+    super().say_id()
+    print("I am an admin.")
+
+# Write your code below
+class Manager(Admin):
+  def say_id(self):
+    print("I am in charge")
+    super().say_id()
+
 e1 = Employee()
 e2 = Employee()
-
-e1.say_id()
-e2.say_id()
+e3 = Admin()
+e4 = Manager()
+e4.say_id()
